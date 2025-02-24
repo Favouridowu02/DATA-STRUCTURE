ca554ae89143c53e01c84d632d9417ea4bb718f0
Favour Idowu <favouridowu02@gmail.com>
2025-02-24 11:06:13 +0100

    feat: Update the module to include an improved version of it

--- sorted_squared_array.js
+++ sorted_squared_array.js
@@ -8,15 +8,38 @@
 function sortedSquaredArray(array) {
     // Create an array of the same length, initialized with zeros
     const newArray = new Array(array.length).fill(0);
-
     // Square each element and store in the new array
     for (let i = 0; i < array.length; i++) {
         newArray[i] = Math.pow(array[i],2);
     }
-
     // Sort the squared values in ascending order
     newArray.sort((a, b) => a - b);
     return newArray;
 }
 
-console.log(sortedSquaredArray([0, 7, 8, -1]));
+
+function sortedSquared(array) {
+    // This is an optimised version
+    const newArray = new Array(array.length).fill(0);
+    leftPointer = 0;
+    rightPointer = array.length - 1;
+    for (let i = array.length - 1; i >= 0; i--) {
+        const leftSquared = Math.pow(array[leftPointer], 2)
+        const rightSquared = Math.pow(array[rightPointer], 2)
+        if (leftSquared > rightSquared) {
+            newArray[i] = leftSquared;
+            leftPointer++;
+        } else {
+            newArray[i] = rightSquared;
+            rightPointer--;
+        }
+    }
+    return newArray;
+}
+
+a = [1, 4, 5, 7]
+b = [-6, -1, 0, 8, 10]
+c = []
+console.log(sortedSquared(a));
+console.log(sortedSquared(b));
+console.log(sortedSquared(c));
