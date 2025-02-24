96a95b7068f7ad4e0953e3085a75cce51501eea1
Favour Idowu <favouridowu02@gmail.com>
2025-02-24 15:11:04 +0100

    feat: Update the module to include an improved version of it

--- sorted_squared.py
+++ sorted_squared.py
@@ -23,3 +23,31 @@ def sorted_squared(array):
     array = [ i**2 for i in array]
     array.sort()
     return array
+
+# This is an optimaised version of the sorted_squared method
+def sorted_squared_array(array):
+    """
+        This function is used to sort the squares of the
+        given array in ascending order.
+        Args:
+            array: List of Integers
+        Returns:
+           List of Integers
+    """
+    new_array = [0]*len(array)
+    left_pointer = 0
+    right_pointer = len(array) - 1
+    
+    for i in reversed(range(len(array))):
+        left_squared = array[left_pointer] ** 2
+        right_squared = array[right_pointer] ** 2
+
+        if left_squared > right_squared:
+            new_array[i] = left_squared
+            left_pointer += 1
+        else:
+            new_array[i] = right_squared
+            right_pointer -= 1
+    return new_array
+
+print(sorted_squared_array([-6, -3, 0, 8, 9]))
