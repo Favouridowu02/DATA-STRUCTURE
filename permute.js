21e552ef717d4cec744a6efff45a0206af11ce4f
Favour Idowu <favouridowu02@gmail.com>
2025-02-28 20:04:01 +0100

    Implement permutation function using backtracking
    - Added `permute` function to find all permutations of an array of distinct integers.
    - Utilized a helper function with backtracking to generate permutations.
    - Swapped elements to explore all possible permutations and reverted swaps to backtrack.
    - Appended each complete permutation to the result list.
    - Ensured the function handles arrays of any length correctly.
    
    This implementation efficiently generates all permutations of the input array using a recursive approach.

--- permute.js
+++ permute.js
@@ -18,3 +18,28 @@ const permute = function(nums) {
     helper(0);
     return result;
 }
+
+const permuteUnique = function(nums) {
+    const result = [];
+    let n = nums.length;
+    function helper(i) {
+        // Base case
+        if (i === n - 1) {
+            result.push([...nums]);
+            return;
+        }
+        // Recursive function
+        let hash = {};
+        for (let j = i; j < n; j++) {
+            if (!hash[nums[j]]) {
+                hash[nums[j]] = true;
+                [nums[i], nums[j]] = [nums[j], nums[i]];
+                helper(i + 1);
+                // backtracking
+                [nums[i], nums[j]] = [nums[j], nums[i]];
+            }
+        }
+    }
+    helper(0);
+    return result;
+}
--- permute.py
+++ permute.py
@@ -24,3 +24,30 @@ def permute(nums):
             nums[j],nums[index] = nums[index],nums[j]
     helper(0)
     return result
+
+
+def permuteUnique(nums):
+    """
+        This function is used to return the unique permute for each values in the arrays
+
+        Arguments:
+            nums - the array of integers
+        
+        Returns: A collection of all the unique possible permutation
+    """
+    result = []
+    def helper(i):
+        # Base case
+        if i == len(nums) - 1:
+            result.append(nums[:])
+            return
+        # Recursive case
+        hash = {}
+        for j in range(i, len(nums)):
+            if nums[j] not in hash:
+                hash[nums[j]]=True
+                nums[i], nums[j] = nums[j], nums[i]
+                helper(i + 1)
+                nums[i], nums[j] = nums[j], nums[i]
+    helper(0)
+    return result
