5c0ccd4d9abe750b9da4882fd003a03dd29a6b2a
Favour Idowu <favouridowu02@gmail.com>
2025-03-05 20:33:13 +0100

    Add a file sudoku that contains a Function solveSudoku to solve sudoku

---
+++ Backtracking Python/sudoku.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    This Module is contains a function that is used to solve a sudoku
+    board using backtracking
+"""
+
+
+def solveSudoku(board):
+    """
+        This Function is used to solve a sudoku board using backtracking
+
+        Arguments:
+            board: An arrary of arrays of integers
+        Return a solve sudoku board
+    """
