c53b8261c6ed6389e8810675d111629ca7f0e122
Favour Idowu <favouridowu02@gmail.com>
2025-03-31 19:53:47 +0100

    feat: Add the Binary search approach for solving the problem og the longest Increasing sequence

--- Dynamic Programming Python/lis.py
+++ Dynamic Programming Python/lis.py
@@ -65,7 +65,7 @@ def lengthOfLIS_1(nums):
     return helper(0, -1)
 
 
-def lengthOfLIS_1(nums):
+def lengthOfLIS_2(nums):
     """
         Tabulation Approach or Bottom Up Approach
         This Function solve the longest Increasing Subsequence Problem
@@ -86,3 +86,58 @@ def lengthOfLIS_1(nums):
                 include = 1 + dp[i+1][i+1]
             dp[i][j] = max(exclude, include)
     return dp[0][0]
+
+
+def lengthOfLIS_3(nums):
+    """
+        Tabulation Approach or Bottom Up Approach Using 1Dimensional Array
+        This Function solve the longest Increasing Subsequence Problem
+
+        Arguments:
+            - nums: An array of integers
+        Space Complexity: O(n)
+        Time Complexity: O(n^2)
+    """
+    n = len(nums)
+    dp = [1]*(n)
+
+    max = 1
+    for i in range(1, n):
+        for j in range(i):
+            if nums[i] > nums[j] and dp[j] +  1 > dp[i]:
+                dp[i] = dp[j + 1]
+        if dp[i]> max: max=dp[i]
+
+    return max
+
+
+def lengthOfLIS_4(nums):
+    """
+        Binary Search Approach
+        This Function solve the longest Increasing Subsequence Problem
+
+        Arguments:
+            - nums: An array of integers
+        Space Complexity: O(nlogn)
+        Time Complexity: O(n)
+    """
+    def binarysearch(sub, num):
+        left, right = 0, len(sub) - 1
+        while left<right:
+            mid = (left+right)//2
+            if sub[mid] < num:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+
+    sub = [nums[0]]
+    n = len(nums)
+
+    for num in nums[1:]:
+        if num>sub[-1]:
+            sub.append(num)
+        else:
+            index = binarysearch(sub, num)
+            sub[index] = num
+    return 
