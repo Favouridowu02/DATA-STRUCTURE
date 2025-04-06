2b91e7c6d9aad7d21d4e45968ec89f82d6dcaf93
Favour Idowu <favouridowu02@gmail.com>
2025-04-06 15:13:13 +0100

    feat: Add a file that contains the function to solve the Longest Palindromic Subsequence Question

---
+++ Dynamic Programming Python/longest_palindrome_sequence.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+    This Module contains a function that contains the solution to Longest Palindromic Substring Question
+
+    Question:
+    Given a string s, find the longest palindromic subsequence's length in s.
+    A subsequence is a sequence that can be derived from another sequence by deleting some or no elements
+    without changing the order of the remaining elements.
+
+    Example :
+    Input: s = "cccacpc"
+    Output: 5
+    Explanation: One possible longest palindromic subsequence is "ccccc".
+"""
+
+
+def longestPalindromeSubseq(s):
+    """
+        Tabulation Approach
+        This Function to find the longest palindromic subsequence in a string.
+
+        Arguments:
+            - s (str): The input string.
+        Returns:
+            - int: The length of the longest palindromic subsequence.
+        Space Complexity: O(n^2)
+        Time complexity: O(n^2)
+    """
+    n = len(s)
+    dp = [[0] * n for _ in range(n)]
+    for l in range(1, n+1):
+        for i in range(n-l+1):
+            j = i + l
+            if i == j:
+                dp[i][j] = 1
+            elif s[i] == s[j]:
+                dp[i][j] = 2 + dp[i+j][j-1]
+            else:
+                dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+
+    return dp[0][n - 1]
